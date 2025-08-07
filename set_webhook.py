2d72d82b8719908cb29a3a374b06b922dd875b9f
ukiokirnos-source <ukio.kirnos@gmail.com>
2025-08-07 17:30:30 +0300

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,7 +1,7 @@
 import requests
 
 bot_token = '4fdbb2493ae7ddc2-cd8869c327e2c592-60fd2dddaa295531'
-webhook_url = 'https://26c3d3d4-d36a-45b4-951e-279afb912a99-00-3mfynntlqse0q.worf.replit.dev/webhook'
+webhook_url = 'https://viberbot-kixl.onrender.com/webhook'
 
 headers = {
     'X-Viber-Auth-Token': bot_token,
