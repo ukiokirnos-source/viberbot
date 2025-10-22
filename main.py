a0c4b0796eb17f819446e1088d60dd24fc35a4bf
ukiokirnos-source <ukio.kirnos@gmail.com>
2025-10-22 17:13:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -254,5 +254,7 @@ def incoming():
 
     return Response(status=200)
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
